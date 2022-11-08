559eb2f4a1108b01b012a5b8273a00daafcff7e3
sandeeppal270 <64672009+sandeeppal270@users.noreply.github.com>
2022-11-08 14:06:45 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import React from "react";
-import { ReactDOM } from "react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const App=()=>{
-   <div>
-    <h1>
-        hello world
-    </h1>
-   </div> 
-}
+const App=()=>(
+    <React.Fragment>
+    <div>
+        <h1>
+            hello world
+        </h1>
+    </div>
+    <div>
+        <Person/>
+        <h2>second hello world</h2>
+    </div>
+    </React.Fragment>
+
+)
+const Person=()=><h1>sandeep pal</h1>;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+)
+
 
